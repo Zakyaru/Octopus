dcbfe815eef5651d38d61eed03b2b8607e5d38e0
Arseni <arseni.ergin@skf.com>
2025-03-17 09:16:19 +0100

    Prononcé de levé de risque OK

--- src/components/Icone.jsx
+++ src/components/Icone.jsx
@@ -1,37 +1,103 @@
-import React from 'react';
+import logoOCTOPUS from "../assets/Logo_V2.png";
+import logoSKF from "../assets/logo_skf.png";
 
 const Icone = ({ id, objet, onClick }) => {
-    const getBackgroundColor = (type) => {
+
+    /*
+    A explorer : possibilité de créer une classe personnalisée "triangle" dans le fichier CSS et ensuite l'utiliser comme Tailwind
+    Avantage actuel : sépare bien la forme et la couleur
+    */
+    const getShapeStyle = (type) => {
         switch (type) {
             case 'création':
-                return 'bg-red-500';
+                return {
+                    borderRadius: '50%', // Cercle
+                };
             case 'test':
-                return 'bg-green-500';
+                return {
+                };
+            case 'fab':
+                return {
+                    borderRadius: '20%', // Carré arrondi
+                };
+            case 'repIn':
+            case 'repOut':
+                return {
+                    clipPath: 'polygon(50% 0%, 0% 100%, 100% 100%)', // Triangle
+                };
+            case 'derog':
+                return {
+                    clipPath: 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)', // Losange
+                };
+            case 'mad':
+                return {
+                    clipPath: 'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)', // Hexagone
+                };
+            case 'image':
+                return {
+                    backgroundImage: `url(${logoOCTOPUS})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                };
+            case 'image2':
+                return {
+                    backgroundImage: `url(${logoSKF})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                };
+            default:
+                return {};
+        }
+    };
+
+    const getBackgroundColorClass = (type) => {
+        switch (type) {
+            case 'création':
+                return 'bg-yellow-500';
+            case 'test':
+                return 'bg-blue-500';
             case 'fab':
                 return 'bg-blue-500';
+            case 'repIn':
+                return 'bg-yellow-500'
+            case 'repOut':
+                return 'bg-green-500'
             case 'derog':
-                return 'bg-yellow-500';
+                return 'bg-orange-500';
             case 'mad':
-                return 'bg-purple-500';
+                return 'bg-green-500';
+            case 'image':
+            case 'image2':
+                return '';
             default:
-                return 'bg-black';
+                return 'bg-gray-500';
         }
     };
 
     return (
         <div
-          className={`${getBackgroundColor(objet.type)} text-white flex items-center justify-center cursor-pointer`}
-          style={{
-            width: 'var(--icon-size)',
-            height: 'var(--icon-size)',
-            marginRight: 'var(--icon-spacing-x)',
-            marginBottom: 'var(--icon-spacing-y)',
-          }}
-          onClick={() => onClick(objet)}
+            style={{
+                marginRight: 'var(--icon-spacing-x-right)',
+                marginLeft: 'var(--icon-spacing-x-left)',
+                marginBottom: 'var(--icon-spacing-y)',
+            }}
         >
-          {id}
+            <div
+                className={`flex cursor-pointer ${getBackgroundColorClass(objet.type)}`}
+                style={{
+                    width: 'var(--icon-size)',
+                    height: 'var(--icon-size)',
+                    ...getShapeStyle(objet.type),
+                }}
+                onClick={() => onClick(objet)}
+            >
+            </div>
+            <div className='bg-gray-100 flex items-center justify-center'>
+                {id + 1}
+            </div>
         </div>
-      );
+
+    );
 };
 
 export default Icone;
